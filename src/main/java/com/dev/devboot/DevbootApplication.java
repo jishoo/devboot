7873a4b6f316caad7b1c367715c216a7a8634217
JS_GALAXYBOOK\jshih <jshihou@gmail.com>
2022-08-18 13:56:29 +0900

    springboot 초기 환경 세팅

---
+++ src/main/java/com/dev/devboot/DevbootApplication.java
@@ -0,0 +1,13 @@
+package com.dev.devboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevbootApplication.class, args);
+	}
+
+}
